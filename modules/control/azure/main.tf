ad07a9cb1c3d6624e99f7a38cd14031120603ad9
rtrentin <105791736+rtrentinavx@users.noreply.github.com>
2026-01-19 16:49:46 -0500

    Feature/spoke included advertised routes (#2)
    * feat(spoke): add route advertisement parameters for transit and BGP peers
    
    * feat(azure): add route options, vHub default route propagation, and spoke time_sleep
    
    - Add propagate_default_route to vwan_hubs (default: true)
    - Add enable_max_performance (default: true) and disable_route_propagation (default: false) to spokes
    - Add bgp_manual_spoke_advertise_cidrs to transits
    - Move spoke_bgp_manual_advertise_cidrs to spoke external connection
    - Add separate time_sleep for spoke vHub connections
    - Update README with new options and vHub limits

--- modules/control/azure/main.tf
+++ modules/control/azure/main.tf
@@ -12,23 +12,23 @@ module "transit" {
   }
 
   transits = {
-    "az-eus2-transit-vnet" = {
+    "az-eaus2-transit-vnet" = {
       cidr                   = "10.1.0.0/23"
       instance_size          = "Standard_D8_v5"
       account                = "lab-test-azure"
-      local_as_number        = 65010
+      local_as_number        = 65021
       fw_amount              = 2
       firewall_image_version = "11.2.5"
       fw_instance_size       = "Standard_D3_v2"
       egress_source_ranges   = ["10.0.0.0/8", "129.222.52.198/32"]
       mgmt_source_ranges     = ["10.0.0.0/8", "129.222.52.198/32"]
       lan_source_ranges      = ["10.0.0.0/8", "129.222.52.198/32"]
-      # vwan_connections = [
-      #   {
-      #     vwan_name     = "vwan-infra"
-      #     vwan_hub_name = "infra"
-      #   }
-      # ]
+      vwan_connections = [
+        {
+          vwan_name     = "vwan-infra"
+          vwan_hub_name = "infra"
+        }
+      ]
       # ssh_keys = ["ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDa2Kz319A3dBeV/bBj5825OGarV5E6zyl70fa3SB2zh2EEsInFY6wj2Dac6nA6vGJTIC5bZPuOhJPsCuniUI+5o4C0df9V8lEQg7PLOcqdeZ3JklfzgvFK/YhWMDQnyJcOxGidVc6ywfyv0h+rbe5V1yhNvudTbvRn84hy/e/RJALBvIT1YUfr98cY+xloH0d/5wWIVtNj37xbwNDA4Eg2qO+84rBHGsIYS6wT+qXNH0IDW2SPQxmnIvf6Sweh2VnlFfn+/lcHhI7XcdjMsYFAKZjdu3ylnWLtbJw4FAY5rL0Q/OAako7pz3OFgGR2al6o/cYVxXjqsfz3yL6Ez32j ricardotrentin@Mac.attlocal.net"]
       enable_password_auth = true
       file_shares = {
@@ -40,52 +40,91 @@ module "transit" {
     }
   }
 
-  # spokes = {
-  #   "az-westus-spoke-vnet" = {
-  #     cidr          = "10.18.0.0/24"
-  #     instance_size = "Standard_D4_v5"
-  #     account       = "lab-test-azure"
-  #     enable_bgp    = false
-  #   }
-  # }
+  spokes = {
+    "az-eaus2-spoke-vnet" = {
+      cidr            = "10.18.0.0/24"
+      instance_size   = "Standard_D4_v5"
+      account         = "lab-test-azure"
+      enable_bgp      = true
+      local_as_number = 65020
+      #included_advertised_spoke_routes = "10.4.0.0/16"
+      #spoke_bgp_manual_advertise_cidrs = ["0.0.0.0/0"]
+      disable_route_propagation = true
+      vwan_connections = [
+        {
+          vwan_name     = "vwan-prod"
+          vwan_hub_name = "prod"
+        }
+      ]
+    },
+    "az-eaus2-spoke-2-vnet" = {
+      cidr            = "10.19.0.0/24"
+      instance_size   = "Standard_D4_v5"
+      account         = "lab-test-azure"
+      enable_bgp      = true
+      local_as_number = 65022
+      #included_advertised_spoke_routes = "10.6.0.0/16"
+      #spoke_bgp_manual_advertise_cidrs = ["0.0.0.0/0"]
+      disable_route_propagation = true
+      vwan_connections = [
+        {
+          vwan_name     = "vwan-non-prod"
+          vwan_hub_name = "non-prod"
+        }
+      ]
+    }
+  }
 
-  # vwan_configs = {
-  #   "vwan-prod" = {
-  #     location            = "East US"
-  #     resource_group_name = "rg-vwan-prod"
-  #     existing            = false
-  #   }
-  #   "vwan-infra" = {
-  #     location            = "East US"
-  #     resource_group_name = "rg-vwan-infra"
-  #     existing            = false
-  #   }
-  # }
+  vwan_configs = {
+    "vwan-prod" = {
+      location            = "East US 2"
+      resource_group_name = "rg-vwan-prod"
+      existing            = false
+    }
+    "vwan-infra" = {
+      location            = "East US 2"
+      resource_group_name = "rg-vwan-infra"
+      existing            = false
+    }
+    "vwan-non-prod" = {
+      location            = "East US 2"
+      resource_group_name = "rg-vwan-non-prod"
+      existing            = false
+    }
+  }
 
-  # vwan_hubs = {
-  #   "infra" = {
-  #     virtual_hub_cidr = "10.2.0.0/24"
-  #     azure_asn        = 65516
-  #   }
-  #   "prod" = {
-  #     virtual_hub_cidr = "10.3.0.0/24"
-  #   }
-  # }
+  vwan_hubs = {
+    "infra" = {
+      virtual_hub_cidr = "10.2.0.0/24"
+    }
+    "prod" = {
+      virtual_hub_cidr = "10.3.0.0/24"
+    }
+    "non-prod" = {
+      virtual_hub_cidr = "10.5.0.0/24"
+    }
+  }
 
-  #   vnets = {
-  #     # "workload1-vnet" = {
-  #     #   cidr            = "10.4.0.0/16"
-  #     #   private_subnets = ["10.4.1.0/24", "10.4.2.0/24"]
-  #     #   public_subnets  = ["10.4.3.0/24", "10.4.4.0/24"]
-  #     #   vwan_name       = "vwan-infra"
-  #     #   vwan_hub_name   = "infra"
-  #     # }
-  #     # "workload2-vnet" = {
-  #     #   resource_group_name = "rg-vnet-workload2-vnet-eastus"
-  #     #   vwan_name           = "vwan-prod"
-  #     #   vwan_hub_name       = "prod"
-  #     #   existing            = true
-  #     # }
-  #   }
-
+  vnets = {
+    "workload1-vnet" = {
+      cidr            = "10.4.0.0/16"
+      private_subnets = ["10.4.1.0/24", "10.4.2.0/24"]
+      public_subnets  = ["10.4.3.0/24", "10.4.4.0/24"]
+      vwan_name       = "vwan-prod"
+      vwan_hub_name   = "prod"
+    },
+    "workload2-vnet" = {
+      cidr            = "10.6.0.0/16"
+      private_subnets = ["10.6.1.0/24", "10.6.2.0/24"]
+      public_subnets  = ["10.6.3.0/24", "10.6.4.0/24"]
+      vwan_name       = "vwan-non-prod"
+      vwan_hub_name   = "non-prod"
+    }
+    # "workload2-vnet" = {
+    #   resource_group_name = "rg-vnet-workload2-vnet-eastus"
+    #   vwan_name           = "vwan-prod"
+    #   vwan_hub_name       = "prod"
+    #   existing            = true
+    # }
+  }
 }
